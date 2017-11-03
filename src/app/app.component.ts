d26696460b1ee21540d22f64d96cab878f4f420d
Cristofer Flores <cristofer.flort@gmail.com>
2017-11-02 19:44:16 -0600

    Added font awesome library and setup the main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+
 declare var $:any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'app';
 
   ngOnInit(){
   	$(document).foundation();
