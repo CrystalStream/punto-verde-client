1728adf57e668f3cbc62e58283d221f6e9083a61
Cristofer Flores <cristofer.flort@gmail.com>
2017-11-02 20:04:44 -0600

    Created the components and added the navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { SectorsComponent } from './sectors/sectors.component';
+import { ScrapsComponent } from './scraps/scraps.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'sectors', component: SectorsComponent },
+  { path: 'scraps', component: ScrapsComponent }
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    HomeComponent,
+    SectorsComponent,
+    ScrapsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
