d3f56c1b03c610c69013e97405c762f8bcab411d
DiegoMosan <diego_z_g@hotmail.com>
2017-11-08 18:05:41 -0600

    Added the sector component

--- src/app/sectors/sectors.route.module.ts
+++ src/app/sectors/sectors.route.module.ts
@@ -1,11 +1,12 @@
+import { AddComponent } from './add/add.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SectorsComponent } from './sectors.component';
-
 const sectorRoutes: Routes = [
-  { path: 'sectors', component: SectorsComponent }
+  { path: 'sectors', component: SectorsComponent },
+  { path: 'sectors/add', component: AddComponent }
 ]
 
 @NgModule({
