d023233d0b0081c3b63f82a555c44e95c8e2eee1
Cristofer <cristofer.flort@gmail.com>
2017-11-19 08:52:11 -0600

    Added more style to the table users

--- src/app/users/detail/detail.component.ts
+++ src/app/users/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
@@ -13,21 +13,21 @@ import { UserService } from '../../shared/api/user.service';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-	// User
-	user: {[key: string]: any};
+  // User
+  user: {[key: string]: any};
 
-	// uuid of user
-	userUuid: Observable<string>;
+  // uuid of user
+  userUuid: Observable<string>;
 
   // loading object
   loading: {[key: string]: any} = {
     all: false,
   };
 
-	// promises array
-	promises: Promise<any>[] = [];
+  // promises array
+  promises: Promise<any>[] = [];
 
-	/*
+  /*
   * constructor
   * @param{UserService} UserService
   * @param{ActivatedRoute} route
@@ -35,27 +35,27 @@ export class DetailComponent implements OnInit {
   constructor(private UserService: UserService, private route: ActivatedRoute, public router: Router) { }
 
   ngOnInit() {
-  	this.route.params.map(p => p.id).subscribe( id => this.userUuid = id);
-  	this.promises.push(this.getUser(this.userUuid))
+    this.route.params.map(p => p.id).subscribe( id => this.userUuid = id);
+    this.promises.push(this.getUser(this.userUuid));
 
-  	Promise.all(this.promises)
-  		.then(() => {
-  			this.loading.all = true;
-  		})
-  		.catch( err => {
-        // console.error(JSON.parse("{Code: '500', message: err, method: 'DetailComponent.ngOnInit()' }"))
+    Promise.all(this.promises)
+      .then(() => {
+        this.loading.all = true;
       })
+    .catch( err => {
+        // console.error(JSON.parse("{Code: '500', message: err, method: 'DetailComponent.ngOnInit()' }"))
+    });
   }
 
   getUser(userId) {
-  	return this.UserService.findOne(userId, 'sector')
-  		.then((response: any) => {
-  			this.user = response.data;
-  			console.log("this.user", this.user);
-  		})
-  		.catch( err => {
-        // console.error(JSON.parse("{Code: '500', message: err, method: 'DetailComponent.getUser()' }"))
+    return this.UserService.findOne(userId, 'sector')
+      .then((response: any) => {
+        this.user = response.data;
+        console.log('this.user', this.user);
       })
+      .catch( err => {
+        // console.error(JSON.parse("{Code: '500', message: err, method: 'DetailComponent.getUser()' }"))
+      });
   }
 
   /*
