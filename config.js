a65c3c92243dde21f35471fb445e196e2d585196
Cristofer <cristofer.flort@gmail.com>
2018-01-14 10:35:49 -0600

    Removed the .env file and added the script before build to read enviromental variables

--- config.js
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-
-dotenv.load();
-console.log('dotenv.load(): ', dotenv.load());
-console.log('process', process.env.BASE_URL)
-
-export const config = {
-  baseUrl: process.env.BASE_URL
-}
---
+++ config.ts
@@ -0,0 +1,32 @@
+import { writeFile } from 'fs';
+import { argv } from 'yargs';
+
+// This is good for local dev environments, when it's better to
+// store a projects environment variables in a .gitignore'd file
+require('dotenv').config();
+
+// Would be passed to script like this:
+// `ts-node set-env.ts --environment=dev`
+// we get it from yargs's argv object
+const environment = argv.environment;
+const isProd = environment === 'prod';
+
+
+const targetPath = `./src/environments/environment.${environment}.ts`;
+
+const envConfigFile = `
+export const environment = {
+  production: ${isProd},
+  baseUrl: ${process.env.BASE_URL},
+  cloudName: ${process.env.CLOUD_NAME},
+  uploadPreset: ${process.env.UPLOAD_PRESET}
+};
+`;
+
+
+writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Configuration setup correctly! Building now...`);
+});
