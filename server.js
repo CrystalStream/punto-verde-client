b1596defc6ae1b0b675ec957bb5e47cb943fc724
Cristofer <cristofer.flort@gmail.com>
2018-01-15 19:30:08 -0600

    Modified the package.json, added a postinstall script and added the forceSSL method in the server.js

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const path = require('path');
 
 const app = express();
 
-// const forceSSL = function() {
-//   console.log(process.env.BASE_URL)
-//   console.log(path.join(__dirname + '/dist/index.html'))
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//        ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   }
-// }
+const forceSSL = function() {
+  console.log(process.env.BASE_URL)
+  console.log(path.join(__dirname + '/dist/index.html'))
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
 
 // Instruct the app
 // to use the forceSSL
 // middleware
-// app.use(forceSSL());
+app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
