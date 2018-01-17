9c0fef9d65589696387e6b9bba1aff9bbbaf6a85
Cristofer <cristofer.flort@gmail.com>
2018-01-16 21:35:54 -0600

    Modified the server js and the outdir of the build to fit heroku requirements

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const path = require('path');
 const app = express();
 
 const forceSSL = function() {
-  console.log(process.env.BASE_URL)
-  console.log(path.join(__dirname + '/dist/index.html'))
-  console.log('path.resolve() with dot', path.resolve('./dist/index.html'));
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
@@ -29,7 +26,7 @@ app.use(express.static('dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile( path.resolve('./dist/index.html') );
+  res.sendFile( path.resolve('/dist/index.html') );
 });
 
 // Start the app by listening on the default
