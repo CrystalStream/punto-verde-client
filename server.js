3e6842f147da0e734aa59ece616f21c287b90a2b
Cristofer <cristofer.flort@gmail.com>
2018-01-16 21:48:16 -0600

    Modified the server.js to get the right path

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const forceSSL = function() {
   return function (req, res, next) {
+    console.log('path.resolve(): ', path.resolve('./dist/index.html'));
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
        ['https://', req.get('Host'), req.url].join('')
@@ -26,7 +27,7 @@ app.use(express.static('dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile( path.resolve('/dist/index.html') );
+  res.sendFile( path.resolve('./dist/index.html') );
 });
 
 // Start the app by listening on the default
